206c25e8d694ab5aab227eff93222e4e409593ec
Razgriz <elchicoconinsomnio@gmail.com>
2014-02-23 01:34:34 +0100

    Creación del repositorio de Dungeon Unity

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Basic Menu Script.
+/// At this version, it just shows a button to exit the game
+/// </summary>
+public class MenuScript : MonoBehaviour {
+
+	void OnGUI()
+	{
+		const int buttonWidth = 80;
+		const int buttonHeight = 50;
+
+		// Draw a button to exit the game
+		if ( GUI.Button( new Rect(30, Screen.height - 80, buttonWidth, buttonHeight), "Exit") )
+		{
+			Application.Quit();
+		}
+
+	}
+
+}
