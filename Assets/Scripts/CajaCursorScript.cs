deeba86fe53fa3dacdf2650dd6f7fa773127353c
Razgriz <elchicoconinsomnio@gmail.com>
2014-03-03 00:01:54 +0100

    Pruebas con texto en el cursor
    Pruebas añadiendo texto al cursor y también una caja para el texto

---
+++ Assets/Scripts/CajaCursorScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CajaCursorScript : MonoBehaviour {
+	
+	//public Transform target;  // Object that this label should follow
+	public Vector3 offset = Vector3.up;    // Units in world space to offset; 1 unit above object by default
+	public bool useMainCamera = true;   // Use the camera tagged MainCamera
+	public Camera cameraToUse ;   // Only use this if useMainCamera is false
+
+	private Vector3 pos;
+
+	Camera cam ;
+	
+	void Start () 
+	{
+		cam = Camera.main;
+	}
+	
+	
+	void Update()
+	{
+		pos = cam.ScreenToWorldPoint(Input.mousePosition + offset);
+		pos.z = -2;
+		this.transform.position = pos; 
+	}
+}
