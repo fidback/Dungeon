322ea619a28fccad8c1e053872c4d3b51a7512c0
Razgriz <elchicoconinsomnio@gmail.com>
2014-03-12 23:46:53 +0100

    Persistencia de datos
    Implementada persistencia de datos según el tutorial de Unity. Escenas
    ControlJuego01 y ControlJuego02. Falta buscar unos datos que necesiten
    ser guardados e incorporarlo al juego.

---
+++ Assets/Scripts/ControlAjusteScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlAjusteScript : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(10, 100, 100, 30), "+ Salud"))
+		{
+			ControlJuegoScript.control.health += 10;
+		}
+		if (GUI.Button(new Rect(10, 140, 100, 30), "- Salud"))
+		{
+			ControlJuegoScript.control.health -= 10;
+		}
+		if (GUI.Button(new Rect(10, 180, 100, 30), "+ Experiencia"))
+		{
+			ControlJuegoScript.control.experience += 10;
+		}
+		if (GUI.Button(new Rect(10, 220, 100, 30), "- Experiencia"))
+		{
+			ControlJuegoScript.control.experience -= 10;
+		}
+		if (GUI.Button(new Rect(10, 260, 100, 30), "Guardar"))
+		{
+			ControlJuegoScript.control.Save();
+		}
+		if (GUI.Button(new Rect(10, 300, 100, 30), "Cargar"))
+		{
+			ControlJuegoScript.control.Load();
+		}
+	}
+}
---
+++ Assets/Scripts/ControlJuegoScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class ControlJuegoScript: MonoBehaviour {
+
+	public static ControlJuegoScript control;
+
+	public float health;
+	public float experience;
+
+
+	void Awake () {
+		if (control == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			control = this;
+		}
+		else if (control != this)
+		{
+			Destroy(gameObject);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.Label(new Rect(10, 10, 100, 30), "Salud: " + health);
+		GUI.Label(new Rect(120, 10, 150, 30), "Experiencia: " + experience);
+	}
+
+	public void Save()
+	{
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+		PlayerData data = new PlayerData();
+		data.health = health;
+		data.experience = experience;
+
+		bf.Serialize(file,data);
+		file.Close();
+	}
+
+	public void Load()
+	{
+		if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+			PlayerData data = (PlayerData)bf.Deserialize(file);
+			file.Close();
+
+			health = data.health;
+			experience = data.experience;
+		}
+	}
+}
+
+[Serializable] // Para que la clase sea serializable (se pueda reducir a un flujo de datos binarios)
+class PlayerData
+{
+	public float health;
+	public float experience;
+}
