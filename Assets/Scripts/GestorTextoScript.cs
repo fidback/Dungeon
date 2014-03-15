36e403a9fb5251be0d7269a9487ef02efea942e2
Razgriz <elchicoconinsomnio@gmail.com>
2014-03-16 00:57:02 +0100

    Textos implementados y build Android probada

---
+++ Assets/Scripts/GestorTextoScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(GUIText))]
+public class GestorTextoScript : MonoBehaviour {
+
+	public static GestorTextoScript text;
+	
+
+	// Use this for initialization
+	void Awake () 
+	{
+		if (text == null)
+		{
+			DontDestroyOnLoad(gameObject);
+			text = this;
+		}
+		else if (text != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	// Funcion para mostrar el texto indicado en pantalla durante la duracion indicada
+	public IEnumerator Show (string m, float t)
+	{
+		if (Debug.isDebugBuild)
+		{
+			Debug.Log("Texto!");
+		}
+		guiText.enabled = true;
+		guiText.text = m;
+		// Vamos a tratar de parar la corutina anterior si la hubiese
+		StopCoroutine("WaitForDisable");
+		yield return StartCoroutine("WaitForDisable", t);
+	}
+
+	public IEnumerator WaitForDisable (float t)
+	{
+		yield return new WaitForSeconds(t);
+		guiText.enabled = false;
+	}
+}
---
+++ Assets/Scripts/ObjetoTextoScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjetoTextoScript : MonoBehaviour {
+
+	// Texto a mostrar
+	public string textToShow;
+
+	// Duracion del texto
+	public float duration;
+	
+	
+	// Cuando se hace clic dentro del Collider
+	void OnMouseDown ()
+	{
+		// Mostramos el texto que se ha indicado
+		if (textToShow.Length != 0)
+		{
+			if (duration > 0f)
+			{
+				StartCoroutine(GestorTextoScript.text.Show(textToShow, duration));
+			}
+			else if (Debug.isDebugBuild)
+			{
+				Debug.Log("<color=blue>No has especificado la duracion! </color>" + duration);
+			}
+		}
+		else
+		{
+			if (Debug.isDebugBuild)
+			{
+				Debug.Log("<color=blue>No has indicado el mensaje a mostrar! </color>" + textToShow);
+			}
+		}
+	}
+	
+	
+	// Establecemos un Gizmo personalizado para estos objetos interactivos
+	void OnDrawGizmos() {
+		Gizmos.DrawIcon(transform.position, "ManoGizmo.png");
+	}
+}
