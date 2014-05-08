bee968a2aced59f6e0b2b0565950ad4d5bf2473a
Razgriz <elchicoconinsomnio@gmail.com>
2014-05-08 22:18:59 +0200

    Cambio cursor al clicar y más sprites de puertas
    - Implementado cambio de cursor al clicar sobre un objeto.
    - Montados el resto de sprites de las puertas (excepto el de la sala
    S00, que no hay render sin él)

--- Assets/Scripts/InteractivoScript.cs
+++ Assets/Scripts/InteractivoScript.cs
@@ -21,6 +21,12 @@ public class InteractivoScript : MonoBehaviour {
 	// Velocidad en frames por segundo a la que queremos animar el cursor
 	public float framesPerSecond;
 
+	// Si muestra otro cursor al clicar
+	public bool textureOnClick = false;
+
+	// Cursor para cuando se clica
+	public Texture2D clickTexture;
+
 	// Punto de la textura que señala donde apuntamos
 	public Vector2 hotSpot = Vector2.zero;
 
@@ -59,7 +65,7 @@ public class InteractivoScript : MonoBehaviour {
 		try
 		{
 			// Volvemos a dejar el cursor por defecto
-			Cursor.SetCursor(null, Vector2.zero, cursorMode);
+			Cursor.SetCursor(null, hotSpot, cursorMode);
 			// Ocultamos el texto del cursor
 			GestorTextoCursorScript.cursor.HideText();
 		}
@@ -79,10 +85,14 @@ public class InteractivoScript : MonoBehaviour {
 
 	// Mientras estamos encima del objeto
 	void OnMouseOver () {
-		// Animamos el cursor usando el resto de (tiempo * fps / frames)
-		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
-		index = index % cursorTexture.Length;
-		Cursor.SetCursor( cursorTexture[ index ], hotSpot, cursorMode);
+		if (!textureOnClick)
+		{
+			// Animamos el cursor usando el resto de (tiempo * fps / frames)
+			int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+			index = index % cursorTexture.Length;
+			Cursor.SetCursor( cursorTexture[ index ], hotSpot, cursorMode);
+		}
+
 	}
 
 
@@ -92,6 +102,26 @@ public class InteractivoScript : MonoBehaviour {
 		{
 			// El gizmo tiene el mismo nombre que la textura basica del cursor
 			Gizmos.DrawIcon(transform.position, cursorTexture[0].name);
+		}
+	}
+
+	// Cuando se hace clic dentro del Collider
+	void OnMouseDown ()
+	{
+		if (clickTexture && textureOnClick)
+		{
+			// Cambiamos el cursor por el indicado en la variable
+			Cursor.SetCursor( clickTexture, hotSpot, cursorMode);
+		}
+	}
+
+	// Cuando se hace clic dentro del Collider
+	void OnMouseUp ()
+	{
+		if (clickTexture && textureOnClick)
+		{
+			// Cambiamos el cursor por el indicado en la variable
+			Cursor.SetCursor( cursorTexture[0], hotSpot, cursorMode);
 		}
 	}
 
